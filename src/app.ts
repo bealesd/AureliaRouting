22d389dcfd1732b718bea73143592ec5e56b4462
adavid <adavid@TAPC043099>
2018-02-09 14:48:11 +0000

    added views

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 export class App {
-  message = 'Hello World!';
+  message = 'AppHeader';
+
+  configureRouter(config, router){
+    config.title = "RouterTest";
+    config.map([
+      // Home | Router Test
+      {route: "", name: "home", moduleId: "index", title: "home"},
+      {route: "about", name: "about", moduleId:"about", title: "about Us"},
+      {route: 'post/:slug', name:'post', moduleId:'post', title: 'View Post'}
+    ]);
+  }
 }
